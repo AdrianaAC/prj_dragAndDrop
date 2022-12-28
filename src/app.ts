e59e00e7ddc0b59f5ff80f28a07d23294cf219bb
Adriana <adrianaalves098@gmail.com>
2022-12-28 19:55:34 +0000

    122

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class ProjectInput {
+  templateEl: HTMLTemplateElement;
+  hostEl: HTMLDivElement;
+  el: HTMLFormElement;
+
+  constructor() {
+    this.templateEl = <HTMLTemplateElement>(
+      document.getElementById("project-input")
+    );
+    this.hostEl = <HTMLDivElement>document.getElementById("app");
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.el = <HTMLFormElement>importedNode.firstElementChild;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostEl.insertAdjacentElement("afterbegin", this.el);
+  }
+}
