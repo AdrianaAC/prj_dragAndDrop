ef6c525ca1be28c902c90db9156e1fd9f57ece5f
Adriana <adrianaalves098@gmail.com>
2023-01-07 02:38:46 +0000

    124

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,61 @@
+//autoBind decorator
+function autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const ogMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = ogMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// Project Input class
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
   el: HTMLFormElement;
+  titleInput: HTMLInputElement;
+  descriptionInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
 
   constructor() {
-    this.templateEl = <HTMLTemplateElement>(
-      document.getElementById("project-input")
-    );
-    this.hostEl = <HTMLDivElement>document.getElementById("app");
+    this.templateEl = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostEl = document.getElementById("app")! as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateEl.content, true);
-    this.el = <HTMLFormElement>importedNode.firstElementChild;
+    this.el = importedNode.firstElementChild as HTMLFormElement;
+    this.el.id = "user-input";
+
+    this.titleInput = <HTMLInputElement>this.el.querySelector("#title");
+    this.descriptionInput = <HTMLInputElement>(
+      this.el.querySelector("#description")
+    );
+    this.peopleInput = <HTMLInputElement>this.el.querySelector("#people");
+
+    this.configure();
     this.attach();
+  }
+ @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInput.value);
+  }
+
+  private configure() {
+    this.el.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
     this.hostEl.insertAdjacentElement("afterbegin", this.el);
   }
 }
+
+const prjInput = new ProjectInput();
